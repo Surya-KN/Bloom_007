897d3ff46c8e2e9d819698ff8f4f85c13abf4d98
Surya K N <95220715+Surya-KN@users.noreply.github.com>
2022-12-10 08:33:14 +0530

    Add files via upload

--- client/src/components/EventInfo.jsx
+++ client/src/components/EventInfo.jsx
@@ -7,7 +7,6 @@ import Backdrop from '@mui/material/Backdrop';
 import Box from '@mui/material/Box';
 import Modal from '@mui/material/Modal';
 import Fade from '@mui/material/Fade';
-
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -15,7 +14,7 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
+import { CardActionArea } from "@mui/material";
 import { useState } from 'react';
 const style = {
   position: 'absolute',
